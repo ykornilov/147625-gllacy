23621582f63296db68f45025c8890a4943265024
Yury Kornilov <kornilovyv81@gmail.com>
2016-02-20 20:59:54 +0300

    fix action for forms

--- js/catalog.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Y on 20.02.2016.
- */
-var productsFilterForm = document.querySelector(".products-filter");
-
-productsFilterForm.addEventListener("submit", function(event) {
-  event.preventDefault();
-});
