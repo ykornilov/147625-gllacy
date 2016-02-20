97c89ead1cc5e75a20605f351276514b87675bce
Yury Kornilov <kornilovyv81@gmail.com>
2016-02-20 18:54:21 +0300

    layout fixed

---
+++ js/catalog.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Y on 20.02.2016.
+ */
+var productsFilterForm = document.querySelector(".products-filter");
+
+productsFilterForm.addEventListener("submit", function(event) {
+  event.preventDefault();
+});
